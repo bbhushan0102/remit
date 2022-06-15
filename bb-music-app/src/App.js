cfbfb8314e912c07495a61289b6776e1a7318bbd
Bharat Bhushan <bharat@Bharats-MacBook-Pro.local>
2022-06-15 09:29:19 +0100

    Router and link added in app.js

--- bb-music-app/src/App.js
+++ bb-music-app/src/App.js
@@ -1,20 +1,45 @@
 import './App.css';
 // import Player from './components/Player';
 import PlayerMain from './components/musicPlayer/PlayerMain';
+import { BrowserRouter, Route, Link, Switch, NavLink, Router } from 'react-router-dom'
 
 import Sidebar from './components/layout/Sidebar'
 import Contentbar from './components/layout/Contentbar';
 import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
+import Search from './components/musicPlayer/Search';
 
 
 function App() {
   return (
+  
     <div className="App">
-      <Header > </Header>
-      <Contentbar > </Contentbar>
+
+<BrowserRouter>
+  <Switch>
+    <Route exact path="/" component={PlayerMain} />
+    <Route exact path="/search" component={Search} />
+  </Switch>
+</BrowserRouter>
+      
+      {/* <Header >
+         </Header>
+
+      <Contentbar >
+         </Contentbar>
       {/* <header className="App-header"> */}
-      <Sidebar> </Sidebar>
+      <Sidebar>
+    <Router>
+    <NavLink className="navbar-item" activeClassName="is-active" to="/">
+              Home
+            </NavLink>
+            <NavLink className="navbar-item" activeClassName="is-active" to="/search">
+             Search
+            </NavLink>
+        
+    </Router>
+    </Sidebar> 
+
         {/* <span>
          <PlayerMain />
        </span> */}
